ddee666abbafeaa66d58b59efa7c0e713ea7b4c1
Sergey Marchukov <msa.sergei@gmail.com>
2014-02-15 15:11:43 +0400

    - init tabBar

---
+++ SMSplitViewController/SMTabBar.h
@@ -0,0 +1,13 @@
+//
+//  SMTabBar.h
+//  SMSplitViewController
+//
+//  Created by Sergey Marchukov on 15.02.14.
+//  Copyright (c) 2014 Sergey Marchukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMTabBar : UIViewController
+
+@end
---
+++ SMSplitViewController/SMTabBar.m
@@ -0,0 +1,18 @@
+//
+//  SMTabBar.m
+//  SMSplitViewController
+//
+//  Created by Sergey Marchukov on 15.02.14.
+//  Copyright (c) 2014 Sergey Marchukov. All rights reserved.
+//
+
+#import "SMTabBar.h"
+
+@interface SMTabBar ()
+
+@end
+
+@implementation SMTabBar
+
+
+@end
