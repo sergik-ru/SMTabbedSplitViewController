c9b27151506f994a3c07240e2c0588e30a867c8c
Sergey Marchukov <msa.sergei@gmail.com>
2014-02-15 15:09:11 +0400

    - start

---
+++ SMSplitViewController/SMSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMSplitViewController.h
+//  SMSplitViewController
+//
+//  Created by Sergey Marchukov on 15.02.14.
+//  Copyright (c) 2014 Sergey Marchukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMSplitViewController : UIViewController
+
+@end
---
+++ SMSplitViewController/SMSplitViewController.m
@@ -0,0 +1,38 @@
+//
+//  SMSplitViewController.m
+//  SMSplitViewController
+//
+//  Created by Sergey Marchukov on 15.02.14.
+//  Copyright (c) 2014 Sergey Marchukov. All rights reserved.
+//
+
+#import "SMSplitViewController.h"
+
+@interface SMSplitViewController ()
+
+@end
+
+@implementation SMSplitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
